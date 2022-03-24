e936c89e2e734470288537ccf5398a3ab3253942
mahmudul165 <mahmudulh883@gmail.com>
2022-03-24 17:53:32 +0600

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,14 +6,7 @@ module.exports = {
   ],
   theme: {
     extend: {},
-    colors: {
-       
-      },
-    
+    colors: {},
   },
   plugins: [],
-}
-
-
-
-
+};
